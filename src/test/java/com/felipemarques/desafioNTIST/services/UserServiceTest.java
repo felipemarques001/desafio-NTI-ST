9fa310065e20e8372b0921122588c1942284284d
Felipe Marques <felipemarquesgg@outlook.com>
2024-03-11 20:15:17 -0300

    refactor(user): removendo uma dependência desnecessária de UserServiceTest

--- src/test/java/com/felipemarques/desafioNTIST/services/UserServiceTest.java
+++ src/test/java/com/felipemarques/desafioNTIST/services/UserServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 
