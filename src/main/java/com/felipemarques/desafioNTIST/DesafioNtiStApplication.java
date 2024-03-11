b4dd5eca1ad234ea62a8e89f2ddcabf6053e2b2c
Felipe Marques <felipemarquesgg@outlook.com>
2024-03-11 17:41:37 -0300

    criação do projeto

---
+++ src/main/java/com/felipemarques/desafioNTIST/DesafioNtiStApplication.java
@@ -0,0 +1,13 @@
+package com.felipemarques.desafioNTIST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNtiStApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNtiStApplication.class, args);
+	}
+
+}
