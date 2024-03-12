be30b387cbbb8dda1d8624e5bd1b4dbe8eae98e3
Felipe Marques <felipemarquesgg@outlook.com>
2024-03-12 05:25:19 -0300

    feat(user): Implementando a verificação de senha no método register da classe UserService

--- src/main/java/com/felipemarques/desafioNTIST/services/UserService.java
+++ src/main/java/com/felipemarques/desafioNTIST/services/UserService.java
@@ -2,6 +2,7 @@ package com.felipemarques.desafioNTIST.services;
 
 import com.felipemarques.desafioNTIST.dtos.UserRegisterDTO;
 import com.felipemarques.desafioNTIST.exceptions.FieldAlreadyInUseException;
+import com.felipemarques.desafioNTIST.exceptions.InvalidPasswordException;
 import com.felipemarques.desafioNTIST.models.User;
 import com.felipemarques.desafioNTIST.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +11,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -24,6 +27,8 @@ public class UserService implements UserDetailsService {
         if(userRepository.findByEmail(dto.email()) != null)
             throw new FieldAlreadyInUseException("email", dto.email());
 
+        validatePassword(dto.password());
+
         User newUser = new User(
                 UUID.randomUUID(),
                 dto.name(),
@@ -37,5 +42,18 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username);
+    }
+
+    private void validatePassword(String password) {
+        Pattern specialCharPattern = Pattern.compile("[!@#$%&*]");
+        Matcher matcher = specialCharPattern.matcher(password);
+
+        if(!password.matches(".*[A-Z].*") ||
+           !password.matches(".*[a-z].*") ||
+           !password.matches(".*\\d.*") ||
+           !matcher.find()) {
+            throw new InvalidPasswordException("The password must contain at least one uppercase letter," +
+                    " one lowercase letter, one number, and one special character.");
+        }
     }
 }
