4670ac60596b092d418e2d4500234074c7f3d9f0
Felipe Marques <felipemarquesgg@outlook.com>
2024-03-15 16:18:13 -0300

    feat(task): implementando a exibição das Task do usuário na página home

--- src/main/java/com/felipemarques/desafioNTIST/controllers/HomeController.java
+++ src/main/java/com/felipemarques/desafioNTIST/controllers/HomeController.java
@@ -1,13 +1,26 @@
 package com.felipemarques.desafioNTIST.controllers;
 
+import com.felipemarques.desafioNTIST.models.Task;
+import com.felipemarques.desafioNTIST.services.TaskService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final TaskService taskService;
+
+    public HomeController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @GetMapping("/home")
-    public String showHomePage() {
+    public String showHomePage(Model model) {
+        List<Task> tasks = taskService.findByUserId();
+        model.addAttribute("tasks", tasks);
         return "home_page";
     }
 }
