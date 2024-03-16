fefe48ce432cb06b932dd827d150969382cc0c6c
Felipe Marques <felipemarquesgg@outlook.com>
2024-03-16 19:06:41 -0300

    style(user): alterando a estrutura HTML e aplicando CSS no arquivo home_page

--- src/main/java/com/felipemarques/desafioNTIST/controllers/HomeController.java
+++ src/main/java/com/felipemarques/desafioNTIST/controllers/HomeController.java
@@ -1,11 +1,14 @@
 package com.felipemarques.desafioNTIST.controllers;
 
 import com.felipemarques.desafioNTIST.models.Task;
+import com.felipemarques.desafioNTIST.models.User;
 import com.felipemarques.desafioNTIST.services.TaskService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -19,8 +22,13 @@ public class HomeController {
 
     @GetMapping("/home")
     public String showHomePage(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<String> namesUser = List.of(user.getName().split(" "));
         List<Task> tasks = taskService.findByUserId();
+
+        model.addAttribute("firstUserName", namesUser.get(0));
         model.addAttribute("tasks", tasks);
+
         return "home_page";
     }
 }
